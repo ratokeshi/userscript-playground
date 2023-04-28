386dbbb632de35b03e7ff94d9954f389ddde3a3c
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 16:13:15 -0700

    add versioning to the script

--- u.Useless/u.01.background.user.js
+++ u.Useless/u.01.background.user.js
@@ -1,17 +1,26 @@
 // ==UserScript==
-// @name        ratokeshi useless Change Background Color
-// @namespace   https://example.com/
-// @description Changes the background color of the web page
+// @name        ratokeshi useless userscript 4 tokeshi.com
+// @namespace   https://tokeshi.com/
+// @description Changes the background color of the web page and add line
 // @include     https://tokeshi.com/*
-// @version     1
-// @grant       none
+// @version     2
+// @grant       GM_notification
 // ==/UserScript==
 
 (function() {
     'use strict';
+    
+    // Check if the script has been updated
+    var currentVersion = GM_info.script.version;
+    var lastVersion = GM_getValue('lastVersion');
+    if (currentVersion != lastVersion) {
+        GM_notification('ratokeshi useless userscript has been updated to version ' + currentVersion, 'info');
+        GM_setValue('lastVersion', currentVersion);
+    }
+
+    // Change background color
     document.body.style.backgroundColor = 'yellow';
-
-
+    
     // Add text line
     var div = document.createElement('div');
     div.innerHTML = 'This is a text line added by the userscript.';
