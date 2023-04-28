288a66c43a696159bbc1c731e5790ddb241e83c9
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 16:48:08 -0700

    change .version

--- u.Useless/u.01.background.user.js
+++ u.Useless/u.01.background.user.js
@@ -3,7 +3,7 @@
 // @namespace   https://tokeshi.com/
 // @description Changes the background color of the web page and add line
 // @include     https://tokeshi.com/*
-// @version     304.28.1
+// @version     304.28.2
 // @grant       GM_notification
 // ==/UserScript==
 
