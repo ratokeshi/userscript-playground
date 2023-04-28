e118b9954e043f7f3ca47e0f268796288e1f86d9
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 14:31:16 -0700

    new useless script

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # userscript-playground
 playground for userscripts
+
+## Trying some new things
+
+### Useless user scripts
---
+++ u.Useless/u.01.background.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Change Background Color
+// @namespace   https://example.com/
+// @description Changes the background color of the web page
+// @include     https://tokeshi.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.body.style.backgroundColor = 'yellow';
+})();
