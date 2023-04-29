05f5166db6c5217cb5fbf650fc9cc8bb3de37ff9
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 17:03:48 -0700

    no update

---
+++ u.Useless/u.01.background.noupdate.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name        ratokeshi useless noupdate
+// @namespace   https://tokeshi.com/
+// @description Changes the background color of the web page and add line
+// @include     https://tokeshi.com/*
+// @version     304.28.1
+// @grant       GM_notification
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+    //// Check if the script has been updated
+    //var currentVersion = GM_info.script.version;
+    //var lastVersion = GM_getValue('lastVersion');
+    //if (currentVersion != lastVersion) {
+    //    GM_notification('ratokeshi useless userscript has been updated to version ' + currentVersion, 'info');
+    //    GM_setValue('lastVersion', currentVersion);
+    //}
+
+    // Change background color
+    document.body.style.backgroundColor = 'green';
+    
+    // Add text line
+    var div = document.createElement('div');
+    div.innerHTML = 'ratokeshi usless userscript was here added by the userscript.';
+    document.body.insertBefore(div, document.body.firstChild);
+})();
