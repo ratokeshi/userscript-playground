fa321c997fdb47fe0a037f34beb18427a783efb5
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 20:57:39 -0700

    comment update add dattime

--- u.Useless/u.01.background.user.js
+++ u.Useless/u.01.background.user.js
@@ -5,7 +5,7 @@
 // @include     https://*.tokeshi.com/*
 // @downloadURL https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
 // @updateURL   https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
-// @version     304.28.91
+// @version     304.28.92
 // @grant       GM_notification
 // ==/UserScript==
 
@@ -15,10 +15,10 @@
     // Check if the script has been updated
     var currentVersion = GM_info.script.version;
     var lastVersion = GM_getValue('lastVersion');
-    if (currentVersion != lastVersion) {
-        GM_notification('ratokeshi useless userscript has been updated to version ' + currentVersion, 'info');
-        GM_setValue('lastVersion', currentVersion);
-    }
+    //if (currentVersion != lastVersion) {
+    //    GM_notification('ratokeshi useless userscript has been updated to version ' + currentVersion, 'info');
+    //    GM_setValue('lastVersion', currentVersion);
+    //}
 
     // Change background color
     document.body.style.backgroundColor = 'red';
@@ -27,4 +27,19 @@
     var div = document.createElement('div');
     div.innerHTML = 'ratokeshi useless userscript was here added by the userscript.';
     document.body.insertBefore(div, document.body.firstChild);
+
+    // Get the current date and time
+    var now = new Date();
+    var dateTime = now.toLocaleString();
+
+    // Create a div element to hold the date and time
+    var div = document.createElement('div');
+    div.style.backgroundColor = '#f2f2f2';
+    div.style.padding = '10px';
+    div.style.fontWeight = 'bold';
+    div.innerHTML = 'Current date and time: ' + dateTime;
+
+    // Add the div element to the top of the page
+    var body = document.getElementsByTagName('body')[0];
+    body.insertBefore(div, body.firstChild);
 })();
