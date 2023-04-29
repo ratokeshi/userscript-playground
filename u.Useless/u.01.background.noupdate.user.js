ff52713eeec7c70da882a7dd09846bbfdfe4cee5
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 21:40:22 -0700

    14

--- u.Useless/u.01.background.noupdate.user.js
+++ u.Useless/u.01.background.noupdate.user.js
@@ -5,7 +5,7 @@
 // @include     https://*.tokeshi.com/*, https://tokeshi.com/*
 // @downloadURL https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
 // @updateURL   https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
-// @version     304.28.13
+// @version     304.28.14
 // @grant       GM_notification
 // ==/UserScript==
 
