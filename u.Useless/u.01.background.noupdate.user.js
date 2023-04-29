3ea331d7b708800db7a3de98d099a065979f964e
Roy Tokeshi <roy.tokeshi@gmail.com>
2023-04-28 21:33:06 -0700

    green

--- u.Useless/u.01.background.noupdate.user.js
+++ u.Useless/u.01.background.noupdate.user.js
@@ -5,7 +5,7 @@
 // @include     https://*.tokeshi.com/*
 // @downloadURL https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
 // @updateURL   https://github.com/ratokeshi/userscript-playground/raw/main/u.Useless/u.01.background.user.js
-// @version     304.28.12
+// @version     304.28.13
 // @grant       GM_notification
 // ==/UserScript==
 
@@ -21,7 +21,7 @@
     //}
 
     // Change background color
-    document.body.style.backgroundColor = 'red';
+    document.body.style.backgroundColor = 'green';
     
     // Add text line
     var div = document.createElement('div');
